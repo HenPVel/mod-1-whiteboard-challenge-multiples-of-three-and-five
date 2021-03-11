85c7e6b640d5fcf6688ba35d056a619b10aff16f
Henry <hvelas93@gmail.com>
2021-03-11 09:40:14 -0500

    commit tree dd3f4983a922e6ff95da200be70e8924a25496c9

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,42 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+    start_array=[*1..limit]
+
+
+    multiple_array = []
+    
+    start_array.each do |x|
+        if x%3.0 == 0 || x%5 == 0
+            multiple_array << x
+        end
+    end
+    
+    if multiple_array.last%3.0 == 0 || multiple_array.last%5.0 == 0
+        multiple_array.pop
+    end
+
+    multiple_array
+end
+
+def sum_multiples(limit)
+    start_array=[*1..limit]
+
+
+    multiple_array = []
+    
+    start_array.each do |x|
+        if x%3.0 == 0 || x%5 == 0
+            multiple_array << x
+        end
+    end
+    
+    if multiple_array.last%3.0 == 0 || multiple_array.last%5.0 == 0
+        multiple_array.pop
+    end
+
+    multiple_array.reduce(0){|sum,num| sum + num}
+end
+
+
